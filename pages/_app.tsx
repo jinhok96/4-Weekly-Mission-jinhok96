7e3e6cbf3332b30eb82e22f7df8c39349b650800
jinhok96 <jinhok96a@gmail.com>
2024-04-02 19:05:42 +0900

    chore: 여러 파일 및 설정 추가 	modified:   .eslintrc.json 	new file:   fonts/font.css 	modified:   pages/_app.tsx 	modified:   pages/_document.tsx 	deleted:    pages/api/hello.ts 	modified:   pages/index.tsx 	deleted:    public/favicon.ico 	new file:   public/images/add-gray10.svg 	new file:   public/images/add-primary.svg 	new file:   public/images/akar-icons_facebook-fill.svg 	new file:   public/images/akar-icons_twitter-fill.svg 	new file:   public/images/akar-icons_youtube-fill.svg 	new file:   public/images/ant-design_instagram-filled.svg 	new file:   public/images/card-1.png 	new file:   public/images/card-2.png 	new file:   public/images/card-3.png 	new file:   public/images/card-4.png 	new file:   public/images/card-5.png 	new file:   public/images/card-6.png 	new file:   public/images/card-7.png 	new file:   public/images/card-8.png 	new file:   public/images/card-9.png 	new file:   public/images/card-no-image.svg 	new file:   public/images/check.svg 	new file:   public/images/close-icon-dark.svg 	new file:   public/images/close.svg 	new file:   public/images/delete.svg 	new file:   public/images/kebab.svg 	new file:   public/images/link.svg 	new file:   public/images/logo.svg 	new file:   public/images/pen.svg 	new file:   public/images/profile-img.png 	new file:   public/images/search-icon.svg 	new file:   public/images/share-facebook.svg 	new file:   public/images/share-kakao.svg 	new file:   public/images/share-link-copy.svg 	new file:   public/images/share.svg 	new file:   public/images/star.svg 	new file:   public/images/user-avatar.png 	deleted:    public/next.svg 	deleted:    public/vercel.svg 	new file:   styles/colors.css 	modified:   styles/globals.css 	new file:   styles/reset.css 	new file:   styles/utility.css 	modified:   tsconfig.json 	new file:   types.d.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
+import "@/styles/reset.css";
 import "@/styles/globals.css";
+import "@/styles/colors.css";
+import "@/styles/utility.css";
+import "@/fonts/font.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import ReactModal from "react-modal";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // 모달이 활성화될 때 애플리케이션의 "#modal-root"를 제외한 나머지 부분에 대한 접근을 차단
+  ReactModal.setAppElement("#modal-root");
+
+  return (
+    <>
+      <Head>
+        <title>Linkbrary</title>
+        <meta name="description" content="Linkbrary" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <ModalProvider>
+        <InputStateContextProvider>
+          <Component {...pageProps} />
+        </InputStateContextProvider>
+      </ModalProvider>
+    </>
+  );
 }
