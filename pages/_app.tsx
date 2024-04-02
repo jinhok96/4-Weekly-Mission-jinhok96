a4ef996e750f21f644c23877fce27c9cf667eb9f
jinhok96 <jinhok96a@gmail.com>
2024-04-02 22:13:09 +0900

    chore: nextjs에 맞춰 코드 수정 	modified:   pages/_app.tsx 	modified:   pages/_document.tsx 	modified:   tsconfig.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
-import "@/styles/reset.css";
-import "@/styles/globals.css";
-import "@/styles/colors.css";
-import "@/styles/utility.css";
-import "@/fonts/font.css";
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import ReactModal from "react-modal";
+import '@/styles/reset.css';
+import '@/styles/globals.css';
+import '@/styles/colors.css';
+import '@/styles/utility.css';
+import '@/fonts/font.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import ReactModal from 'react-modal';
+
+import { InputStateContextProvider } from '@/contexts/InputStateProvider';
+import ModalProvider from '@/contexts/ModalProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   // 모달이 활성화될 때 애플리케이션의 "#modal-root"를 제외한 나머지 부분에 대한 접근을 차단
-  ReactModal.setAppElement("#modal-root");
+  ReactModal.setAppElement('#modal-root');
 
   return (
     <>
