a5cbc9e3f9eb693de954e61317a1dff417f1b2cb
jinhok96 <jinhok96a@gmail.com>
2024-04-03 17:33:10 +0900

    chore: 카드 컴포넌트 ReactComponent -> SvgUrl로 변경 	modified:   components/layouts/main/card/Card.tsx

--- components/layouts/main/card/Card.tsx
+++ components/layouts/main/card/Card.tsx
@@ -9,7 +9,7 @@ import SelectMenu from '@/components/common/SelectMenu';
 import KebabButton from '@/components/common/buttons/KebabButton';
 import StarButton from '@/components/common/buttons/StarButton';
 import styles from '@/components/layouts/main/card/Card.module.css';
-import { ReactComponent as NoImageSvg } from '@/public/images/card-no-image.svg';
+import noImageSvgUrl from '@/public/images/card-no-image.svg';
 
 const cardClasses = classNames(styles.card, 'position-relative');
 const hiddenSelectMenuClasses = classNames('display-none');
@@ -42,7 +42,7 @@ function Card({ linkData }: CardProps) {
   const createdDate = formatDate(createdAt);
 
   const backgroundImage = {
-    background: `#d9d9d9 url(${imageSource || NoImageSvg}) no-repeat center / cover`,
+    background: `#d9d9d9 url(${imageSource || noImageSvgUrl}) no-repeat center / cover`,
   };
 
   const handleCardClick = () => {
