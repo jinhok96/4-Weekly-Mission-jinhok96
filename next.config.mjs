ef38828abc87b5b90b333261e899798aab3d9d17
jinhok96 <jinhok96a@gmail.com>
2024-04-03 16:51:58 +0900

    chore: svg를 ReactComponent로 변환하는 설정 추가 	modified:   next.config.mjs 	modified:   tsconfig.json 	new file:   typing.d.ts

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack', 'url-loader'],
+      use: ['@svgr/webpack'],
     });
 
     return config;
