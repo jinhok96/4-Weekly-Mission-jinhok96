7d6af1f92d9274c484f3855146f27a22e08aa172
jinhok96 <jinhok96a@gmail.com>
2024-04-09 16:54:29 +0900

    chore: text prop을 children으로 통합 	modified:   components/common/buttons/Button.tsx

--- components/common/buttons/Button.tsx
+++ components/common/buttons/Button.tsx
@@ -5,15 +5,13 @@ import styles from '@/components/common/buttons/Button.module.css';
 
 interface ButtonProps extends React.ComponentProps<'button'> {
   className?: string;
-  text?: string;
 }
 
-function Button({ className = '', text = '', onClick, name = '', children = null }: ButtonProps) {
+function Button({ className = '', onClick, name = '', children = null }: ButtonProps) {
   const buttonClasses = classNames(styles.button, 'cursor-pointer', className);
 
   const button = (
     <button className={buttonClasses} type="button" name={name} onClick={onClick}>
-      {text}
       {children}
     </button>
   );
