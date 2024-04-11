aa2b8e39a8bb65d436b9b9dd6825e3194964f5d5
jinhok96 <jinhok96a@gmail.com>
2024-04-11 18:36:22 +0900

    chore: shared 페이지 개선 	modified:   pages/shared/index.module.css 	modified:   pages/shared/index.tsx

--- pages/shared/index.tsx
+++ pages/shared/index.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import React, { useEffect, useState } from 'react';
 
 import AsyncBoundary from '@/components/common/AsyncBoundary';
 import ErrorMessage from '@/components/common/ErrorMessage';
@@ -9,14 +10,34 @@ import Main from '@/components/layouts/main/Main';
 import CardList from '@/components/layouts/main/card/CardList';
 import SearchBar from '@/components/layouts/main/searchBar/SearchBar';
 import HeaderContent from '@/pages/shared/headerContent/HeaderContent';
-
-// import styles from '@/pages/SharedPage/SharedPage.module.css';
+import styles from '@/pages/shared/index.module.css';
+import selectParticle from '@/utils/selectPostposition';
 
 const containerClasses = classNames('min-height-100vh');
+const searchResultTitleClasses = classNames(styles['search-result-title'], 'text-color-gray60');
 
 function Shared() {
+  const [inputState, setInputState] = useState('');
+  const [isSearchResultVisible, setIsSearchResultVisible] = useState(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputState(e.target.value);
+  };
+
+  const handleInputResetClick = () => {
+    setInputState('');
+  };
+
+  useEffect(() => {
+    if (inputState) {
+      setIsSearchResultVisible(true);
+    } else {
+      setIsSearchResultVisible(false);
+    }
+  }, [inputState]);
+
   return (
-    <div>
+    <AsyncBoundary>
       <div className={containerClasses}>
         <Header>
           <AsyncBoundary errorFallback={<ErrorMessage message="Error" />} loadingFallback={<LoadingMessage />}>
@@ -24,12 +45,18 @@ function Shared() {
           </AsyncBoundary>
         </Header>
         <Main>
-          <SearchBar />
-          <CardList />
+          <SearchBar value={inputState} onChange={handleInputChange} onInputReset={handleInputResetClick} />
+          {isSearchResultVisible && (
+            <p className={searchResultTitleClasses}>
+              <span className="text-color-gray100">{inputState}</span>
+              {selectParticle(inputState)} 검색한 결과입니다.
+            </p>
+          )}
+          <CardList folderId={0} filter={inputState} />
         </Main>
       </div>
       <Footer />
-    </div>
+    </AsyncBoundary>
   );
 }
 
